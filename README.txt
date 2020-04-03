b09724ab494e9f6949c8446234a4ce9f15637414
Reza Moradi <rezamoradi83r@gmail.com>
2020-04-03 16:08:24 +0430

    first commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+a simple code thats type words in your clipboard
+
+کد ساده ای برای چاپ کردن متن های موجود در کلیپ بورد
---
+++ clipboard.py
@@ -0,0 +1,12 @@
+#Made by Reza Moradi
+
+import pyperclip
+import time
+
+while True:
+    a=pyperclip.paste()
+    print(a)
+    time.sleep(3)
+
+
+
